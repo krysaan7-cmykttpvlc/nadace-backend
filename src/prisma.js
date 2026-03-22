c76c5b48856e2cfa38aefc8300a4fa4515e7d19f
Mates <krysaan7@example.com>
2026-03-22 23:20:01 +0100

    Downgrade to Prisma 6 for Railway compatibility

--- src/prisma.js
+++ src/prisma.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: require('path').join(__dirname, '..', '.env') });
-const { PrismaClient } = require('../generated/prisma');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
